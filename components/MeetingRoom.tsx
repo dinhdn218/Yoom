04da41a18ef878203690c54c1962779cb9645562
DinhDN <dinhdn218@gmail.com>
2024-11-09 03:31:42 +0700

    create meeting setup

---
+++ components/MeetingRoom.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MeetingRoom = () => {
+  return (
+    <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
+      MeetingRoom
+    </section>
+  );
+};
+
+export default MeetingRoom;
