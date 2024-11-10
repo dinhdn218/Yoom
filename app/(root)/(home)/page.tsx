4ec1c9425e1d6fe819bf14bcac35abdf6a160242
DinhDN <dinhdn218@gmail.com>
2024-11-11 04:05:16 +0700

    add feature upcoming, previous, recordings

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,25 +1,33 @@
+"use client";
+
 import MeetingTypeList from "@/components/MeetingTypeList";
+import { useGetCalls } from "@/hooks/useGetCalls";
 import React from "react";
+import Clock from "react-live-clock";
 
 const Home = () => {
   const now = new Date();
-  const time = now.toLocaleTimeString("en-US", {
-    hour: "2-digit",
-    minute: "2-digit",
-  });
   const date = new Intl.DateTimeFormat("en-US", {
     dateStyle: "full",
   }).format(now);
+  const { upcomingCalls } = useGetCalls();
 
   return (
     <section className="flex size-full flex-col gap-10 text-white">
       <div className="h-[300px] w-full rounded-[20px] bg-home bg-cover">
         <div className="flex  h-full flex-col justify-between max-lg:px-5 max-lg:py-8 lg:p-11">
-          <h2 className="glassmorphism max-w-[290px] rounded py-2 text-center text-base font-normal">
-            Upcoming Meeting at: 12:30 PM
-          </h2>
+          {!upcomingCalls || upcomingCalls.length === 0 ? null : (
+            <h2 className="glassmorphism max-w-[290px] rounded py-2 text-center text-base font-normal">
+              Upcoming Meeting at:{" "}
+              {upcomingCalls?.[
+                upcomingCalls?.length - 1
+              ].state?.startsAt?.toLocaleString()}
+            </h2>
+          )}
           <div className="flex flex-col gap-2">
-            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
+            <h1 className="text-4xl font-extrabold lg:text-7xl">
+              <Clock format="HH:mm:ss" interval={1000} ticking={true} />
+            </h1>
             <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
           </div>
         </div>
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -9,6 +9,7 @@ import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
 import { useToast } from "@/hooks/use-toast";
 import { Textarea } from "./ui/textarea";
 import ReactDatePicker from "react-datepicker";
+import { Input } from "./ui/input";
 
 const MeetingTypeList = () => {
   const { toast } = useToast();
@@ -82,6 +83,7 @@ const MeetingTypeList = () => {
         handleClick={() => setMeetingState("isInstantMeeting")}
         className="bg-orange-1"
       />
+
       <HomeCard
         img="/icons/schedule.svg"
         title="Schedule Meeting"
@@ -94,9 +96,10 @@ const MeetingTypeList = () => {
         img="/icons/recordings.svg"
         title="View Recordings"
         description="Start an instant meeting"
-        handleClick={() => setMeetingState("isJoiningMeeting")}
+        handleClick={() => router.push("/recordings")}
         className="bg-purple-1"
       />
+
       <HomeCard
         img="/icons/join-meeting.svg"
         title="Join Meeting"
@@ -180,6 +183,26 @@ const MeetingTypeList = () => {
         buttonText="Start Meeting"
         handleClick={createMeeting}
       />
+
+      <MeetingModal
+        isOpen={meetingState === "isJoiningMeeting"}
+        onClose={() => setMeetingState(undefined)}
+        title="Type the link here"
+        className="text-center"
+        buttonText="Join Meeting"
+        handleClick={() => router.push(values.link)}
+      >
+        <Input
+          placeholder="Meeting link"
+          className="border-none bg-dark-2 focus-visible:ring-offset-0 focus-visible:ring-0"
+          onChange={(e) => {
+            setValues({
+              ...values,
+              link: e.target.value,
+            });
+          }}
+        />
+      </MeetingModal>
     </section>
   );
 };
